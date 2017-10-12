7566b3696176c33c39987b7d6819503735245622
Kristoffer Føllesdal <kristoffer@Kristoffers-iMac.local>
2017-10-12 13:07:22 +0200

    Update monoid test

--- test/Math/MonoidTest.hs
+++ test/Math/MonoidTest.hs
@@ -7,10 +7,10 @@
 
 {-
 Module      : Math.MonoidTest
-Description : Test for monoid implementations. 
+Description : Test for monoid implementations.
 Maintainer  : kfo021@uib.no
 
-Test for monoid implementations. 
+Test for monoid implementations.
 -}
 
 module Math.MonoidTest where
@@ -25,14 +25,14 @@ import Test.Tasty.QuickCheck as QC
 import Test.SmallCheck.Series (Serial)
 
 import Math.Properties.Elements (unit)
-import Math.Properties.Operations (assosiative)
+import Math.Properties.Operations (assosiative, commutative)
 
 
 
 data Type a = Test
 
-monoid :: forall a . (Eq a, Serial IO a, Show a) => Type a -> (a -> a -> a) -> a -> [TestTree]
-monoid _ op u = [SC.testProperty "unit laws" $ unit @a op u
+monoidSC :: forall a . (Eq a, Serial IO a, Show a) => Type a -> (a -> a -> a) -> a -> [TestTree]
+monoidSC _ op u = [SC.testProperty "unit laws" $ unit @a op u
                 ,SC.testProperty "assoiativity law" $ assosiative @a op]
 -- ,localOption (4 :: SmallCheckDepth) $
 
@@ -40,8 +40,28 @@ monoidQC :: forall a . (Eq a, Arbitrary a, Show a)=> Type a -> (a -> a -> a) -> 
 monoidQC _ op u = [QC.testProperty "unit laws" $ unit @a op u
                   ,QC.testProperty "assoiativity law" $ assosiative @a op]
 
-test_Math_Monoid = [testGroup "Monoid (Integer,*,1) [SmallCheck]" $ monoid (Test :: Type Integer) (*) u
-                   ,testGroup "Monoid (Integer,*,1) [QuickCheck]" $ monoidQC (Test :: Type Integer) (*) u
-                   ,testGroup "Monoid (Int,*,1) [SmallCheck]" $ monoid (Test :: Type Int)  (*) u
-                   ,testGroup "Monoid (Int,*,1) [QuickCheck]" $ monoidQC (Test :: Type Int)  (*) u]
+commutativeMonoidSC :: forall a . (Eq a, Serial IO a, Show a) => Type a -> (a -> a -> a) -> a -> [TestTree]
+commutativeMonoidSC t op u = [testGroup "Monoid" $ monoidSC t op u,
+                            SC.testProperty "commutative law" $ commutative @a op]
 
+commutativeMonoidQC :: forall a . (Eq a, Arbitrary a, Show a) => Type a -> (a -> a -> a) -> a -> [TestTree]
+commutativeMonoidQC t op u = [testGroup "Monoid" $ monoidQC t op u,
+                              QC.testProperty "commutative law" $ commutative @a op]
+
+scCommutativeMonoid :: TestTree
+scCommutativeMonoid = testGroup "[SmallCheck]"
+  [testGroup "(Integer,*,1)" $ commutativeMonoidSC (Test :: Type Integer) (*) u,
+   testGroup "(Int,*,1)" $ commutativeMonoidSC (Test :: Type Int) (*) u
+  ]
+
+qcCommutativeMonoid :: TestTree
+qcCommutativeMonoid = testGroup "[QuickCheck]"
+  [testGroup "(Integer,*,1)" $ commutativeMonoidQC (Test :: Type Integer) (*) u,
+   testGroup "(Int,*,1)" $ commutativeMonoidQC (Test :: Type Int) (*) u
+  ]
+
+test_monoids :: TestTree
+test_monoids = testGroup "Monoids" [testGroup "([Char],++,[])" $ monoidQC (Test :: Type [Char]) (++) []]
+
+test_commutativMonoid :: TestTree
+test_commutativMonoid = testGroup "Commutative Monoids" [scCommutativeMonoid, qcCommutativeMonoid]
