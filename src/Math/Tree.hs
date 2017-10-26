5e48ffddf1e228ee7e52c0955c6cb389fc037369
Kristoffer Føllesdal <kristoffer@Kristoffers-iMac.local>
2017-10-26 22:40:24 +0200

    Some messing around on the Tree.hs

--- src/Math/Tree.hs
+++ src/Math/Tree.hs
@@ -2,7 +2,9 @@
   NoImplicitPrelude,
   UnicodeSyntax,
   TypeFamilies,
-  ViewPatterns
+  ViewPatterns,
+  FlexibleInstances,
+  MultiParamTypeClasses
 #-}
 
 -- FreeModule Monad
@@ -10,10 +12,11 @@
 
 module Math.Tree where
 
-import GHC.Base (Eq (..), Ord (..), Bool (..), error, ($),(++))
+import GHC.Base (Eq (..), Ord (..), Bool (..), error, ($),(++), undefined, Int)
 import Text.Show (Show(..))
 import Math.Module
 import Math.Module.FreeModule
+import Data.Monoid
 
 data MK c = I | MK {root:: c, left:: MK c, rigth :: MK c} deriving (Eq, Ord, Show)
 
@@ -29,20 +32,60 @@ instance Forrest MK where
   bMinus x = error $ show x ++ "Is not a Tree"
   mk_ c l r = MK c l r
   mkSplit (MK c l r) = (c,l,r)
+  mkSplit I = (undefined,I,I)
+  concat I x = x
+  concat x I = x
+  concat (MK c l r) (MK c2 l2 r2) = MK c l r2
+  empty = I
+  lift x = x
   --unconcat I = (I,I)
   --unconcat (MK c l r) =
 
-class Forrest f where
+
+-- evenetuekt (Monoid (f c)) => Forrest f c
+class Forrest f where -- Monad
     type Tree f c ∷ *
     isTree ∷ f c -> Bool
     isEmpty ∷ f c -> Bool
     bPlus ∷ c -> f c → Tree f c
     bMinus ∷ (Show c) ⇒ Tree f c -> f c
     mk_ ∷ c -> f c -> f c -> f c
+    --mk_ c v u = concat v (bPlus c u)
     --mk ∷ c -> FreeModule k (f c) → FreeModule k (f c) → FreeModule k (f c)
 
     mkSplit ∷ f c -> (c, f c,f c)
+    mkSplit x = (mkRoot x, mkLeft x, mkRigth x)
+    mkRoot :: f c -> c
+    mkRoot (mkSplit -> (c,_,_)) = c
+    mkLeft :: f c -> f c
+    mkLeft (mkSplit -> (_,l,_)) = l
+    mkRigth :: f c -> f c
+    mkRigth (mkSplit -> (_,_,r)) = r
+    concat :: f c -> f c -> f c
+    empty :: f c
+    lift :: Tree f c -> f c
     --unconcat ∷ f c -> (Tree f c, f c)
+
+class LeftConcat a b f where -- Can we use Module?
+  lconcat :: a -> b -> f
+
+instance (Forrest f) => LeftConcat (f c) (f c) (f c) where
+  lconcat u v = concat u v
+
+instance (Forrest f, u ~ Tree f c, v ~ Tree f c) => LeftConcat u v (f c) where
+  lconcat t f = concat (lift t) (lift f)
+
+instance (Forrest f, u ~ Tree f c) => LeftConcat u (f c) (f c) where
+  lconcat t f = concat (lift t) f
+
+instance (Forrest f, u ~ Tree f c) => LeftConcat (f c) u (f c) where
+  lconcat f t = concat f (lift t)
+
+
+instance (Forrest f) => Monoid (f c) where
+  mempty = empty
+  mappend = concat
+
 
 -- class GraftingProduct f where
 --   graft_, (⊵) :: f → f → FreeModule k f
