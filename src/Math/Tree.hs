2ddba5ebb69d7b65dd3af428c78af26dd308e16b
Kristoffer Føllesdal <kfollesdal@gmail.com>
2017-11-02 14:59:05 +0100

    Some work on Tree.hs

--- src/Math/Tree.hs
+++ src/Math/Tree.hs
@@ -13,63 +13,110 @@
 
 module Math.Tree where
 
-import GHC.Base (Eq (..), Ord (..), Bool (..), error, ($),(++), undefined, Int)
+import GHC.Base (id, Eq (..), Ord (..), Bool (..), otherwise, error, ($),(++), undefined, Int)
 import Text.Show (Show(..))
 import Math.Module
 import Math.Module.FreeModule
 import Data.Monoid
-import Data.Text
+
+class Enumerable e where
+  count :: e -> Int
+
+class (Eq e) => HasEmpty e where
+  empty :: e
+  isEmpty :: e -> Bool
+  isEmpty x
+    | x == empty = True
+    | otherwise = False
+
+class (HasEmpty c) => Collection c where
+  type Element c
+  insert :: Element c -> c -> c
+  singelton :: Element c -> c
+  singelton x = insert x empty
+
+instance (Eq a) => HasEmpty [a] where
+  empty = []
+
+instance (Eq a) => Collection [a] where
+  type Element [a] = a
+  insert x ce = x:ce
+
+class (Enumerable c, Collection c) => EnumerableCollection c where
+  toList :: c -> [Element c]
+
+class Tree t where
+  type Node t
+
+class (Collection f, Tree(Element f), Monoid f) => Forrest f where
+  isTree :: f -> Bool
+  bplus :: Node (Element f) -> f -> Element f
+  bminus :: Element f -> f
+  mk_ :: Node(Element f) -> f -> f -> f
+  mkSplit :: f -> (Node(Element f),f,f)
+  mkSplit w = (mkRoot w, mkLeft w, mkRigth w)
+  mkRoot :: f -> Node(Element f)
+  mkRoot (mkSplit -> (c,_,_)) = c
+  mkLeft :: f -> f
+  mkLeft (mkSplit -> (_,l,_)) = l
+  mkRigth :: f -> f
+  mkRigth (mkSplit -> (_,_,r)) = r
+  mk :: Node(Element f) -> FreeModule k f -> FreeModule k f -> FreeModule k f
+  --mk = bilinear mk_
+  --unconcat :: f -> (Tree f, f)
+
+
 
 data MK c = I | MK {root:: c, left:: MK c, rigth :: MK c} deriving (Eq, Ord, Show)
 
-instance Forrest MK c  where
-  type Tree MK c = MK c
-  isTree I = False
-  isTree (MK c I x) = True
-  isTree (MK c x y) = False
-  isEmpty I = True
-  isEmpty x = False
-  bPlus c x = MK c I x
-  bMinus (MK c I f) = f
-  bMinus x = error "Is not a Tree"
-  mk_ c l r = MK c l r
-  mkSplit (MK c l r) = (c,l,r)
-  mkSplit I = (undefined,I,I)
-  concat I x = x
-  concat x I = x
-  concat (MK c l r) (MK c2 l2 r2) = MK c l r2
-  empty = I
-  lift x = x
-  --unconcat I = (I,I)
-  --unconcat (MK c l r) =
-
-class TreeC a where
-
-instance TreeC (MK c)
-
--- evenetuekt (Monoid (f c)) => Forrest f c
-class (TreeC(Tree f c)) => Forrest f c where -- Monad
-    type Tree f c ∷ *
-    isTree ∷ f c -> Bool
-    isEmpty ∷ f c -> Bool
-    bPlus ∷ c -> f c -> Tree f c
-    bMinus ∷ Tree f c -> f c
-    mk_ ∷ c -> f c -> f c -> f c
-    --mk_ c l r = concat l (bPlus c r)
-    --mk ∷ c -> FreeModule k (f c) → FreeModule k (f c) → FreeModule k (f c)
-    -- mk = bilinear mk_
-    mkSplit ∷ f c -> (c, f c,f c)
-    mkSplit x = (mkRoot x, mkLeft x, mkRigth x)
-    mkRoot :: f c -> c
-    mkRoot (mkSplit -> (c,_,_)) = c
-    mkLeft :: f c -> f c
-    mkLeft (mkSplit -> (_,l,_)) = l
-    mkRigth :: f c -> f c
-    mkRigth (mkSplit -> (_,_,r)) = r
-    concat :: f c -> f c -> f c
-    empty :: f c
-    lift :: Tree f c -> f c
-    --unconcat ∷ f c -> (Tree f c, f c)
+-- instance Forrest MK c  where
+--   type Tree MK c = MK c
+--   isTree I = False
+--   isTree (MK c I x) = True
+--   isTree (MK c x y) = False
+--   isEmpty I = True
+--   isEmpty x = False
+--   bPlus c x = MK c I x
+--   bMinus (MK c I f) = f
+--   bMinus x = error "Is not a Tree"
+--   mk_ c l r = MK c l r
+--   mkSplit (MK c l r) = (c,l,r)
+--   mkSplit I = (undefined,I,I)
+--   concat I x = x
+--   concat x I = x
+--   concat (MK c l r) (MK c2 l2 r2) = MK c l r2
+--   empty = I
+--   lift x = x
+--   --unconcat I = (I,I)
+--   --unconcat (MK c l r) =
+--
+-- class TreeC a where
+--
+-- instance TreeC (MK c)
+--
+-- -- evenetuekt (Monoid (f c)) => Forrest f c
+-- class (TreeC(Tree f c)) => Forrest f c where -- Monad
+--     type Tree f c ∷ *
+--     isTree ∷ f c -> Bool
+--     isEmpty ∷ f c -> Bool
+--     bPlus ∷ c -> f c -> Tree f c
+--     bMinus ∷ Tree f c -> f c
+--     mk_ ∷ c -> f c -> f c -> f c
+--     --mk_ c l r = concat l (bPlus c r)
+--     --mk ∷ c -> FreeModule k (f c) → FreeModule k (f c) → FreeModule k (f c)
+--     -- mk = bilinear mk_
+--     mkSplit ∷ f c -> (c, f c,f c)
+--     mkSplit x = (mkRoot x, mkLeft x, mkRigth x)
+--     mkRoot :: f c -> c
+--     mkRoot (mkSplit -> (c,_,_)) = c
+--     mkLeft :: f c -> f c
+--     mkLeft (mkSplit -> (_,l,_)) = l
+--     mkRigth :: f c -> f c
+--     mkRigth (mkSplit -> (_,_,r)) = r
+--     concat :: f c -> f c -> f c
+--     empty :: f c
+--     lift :: Tree f c -> f c
+--     --unconcat ∷ f c -> (Tree f c, f c)
 
 
 -- instance (Forrest f) => Monoid (f c) where
