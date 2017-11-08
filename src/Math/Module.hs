d780c043607a5556456ffb020b74e74c129b54d4
Kristoffer Føllesdal <kfollesdal@gmail.com>
2017-11-08 15:14:17 +0100

    Rewrite tf

--- src/Math/Module.hs
+++ src/Math/Module.hs
@@ -78,8 +78,9 @@ class (Module m, Module n,
   --    = msum [(a*b) *> basis (x,y) | (x,a) <-xs, (y,b) <- ys]
   tf :: (Scalar (Tensor m n) ~ Scalar (Tensor m' n'), HasTensorProduct m' n')
       => (m -> m') -> (n -> n') -> Tensor m n -> Tensor m' n'
-  tf f g (decompose -> xs)
-     = msum [k *> te (f (basis x)) (g (basis y)) | ((x,y),k) <- xs]
+  tf f g = linear (\(x,y) -> te (f (basis x)) (g (basis y)))
+  -- tf f g (decompose -> xs)
+  --    = msum [k *> te (f (basis x)) (g (basis y)) | ((x,y),k) <- xs]
 
 -- -- HasBasis => FreeModule
 -- class (Functor f, Monad f) => FreeModule f where
