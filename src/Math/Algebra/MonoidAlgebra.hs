63b970ec22c8cd73234ce0e32b063cc326af4ead
Kristoffer Føllesdal <kfollesdal@gmail.com>
2017-11-09 14:14:06 +0100

    Started file for MonoidAlgebra

---
+++ src/Math/Algebra/MonoidAlgebra.hs
@@ -0,0 +1,20 @@
+-- STARTED not added to project
+
+{-# LANGUAGE
+  NoImplicitPrelude
+#-}
+
+module Math.Alegebra.MonoidAlgebra where
+
+import Math.Algebra
+import Math.Monoid
+import Math.Ring
+import Math.Module
+import Math.Module.FreeModule
+import GHC.Base (Eq (..), Ord (..))
+
+monoidAlgebra :: (MultiplicativeMonoid b, CommutativeRing k, Eq k, Ord b) => AlgebraD (FreeModule k b)
+monoidAlgebra = AlgebraD {
+  unitD = \x -> x *> basis u,
+  multD = linear (\(x,y) -> basis (x*y))
+}
