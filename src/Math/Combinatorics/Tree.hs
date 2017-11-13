d617c4a397278c924be20922f0b3040b6774c1d1
Kristoffer Føllesdal <kristoffer@Kristoffers-iMac.local>
2017-11-13 21:25:08 +0100

    Some more work on Tree.hs

--- src/Math/Combinatorics/Tree.hs
+++ src/Math/Combinatorics/Tree.hs
@@ -5,7 +5,8 @@
   ViewPatterns,
   FlexibleInstances,
   MultiParamTypeClasses,
-  FlexibleContexts
+  FlexibleContexts,
+  TypeOperators
 #-}
 
 -- FreeModule Monad
@@ -18,32 +19,9 @@ import Text.Show (Show(..))
 import Math.Algebra.Module
 import Math.Algebra.Module.FreeModule
 import Data.Monoid
-
-class Enumerable e where
-  count :: e -> Int
-
-class (Eq e) => HasEmpty e where
-  empty :: e
-  isEmpty :: e -> Bool
-  isEmpty x
-    | x == empty = True
-    | otherwise = False
-
-class (HasEmpty c) => Collection c where
-  type Element c
-  insert :: Element c -> c -> c
-  singelton :: Element c -> c
-  singelton x = insert x empty
-
-instance (Eq a) => HasEmpty [a] where
-  empty = []
-
-instance (Eq a) => Collection [a] where
-  type Element [a] = a
-  insert x ce = x:ce
-
-class (Enumerable c, Collection c) => EnumerableCollection c where
-  toList :: c -> [Element c]
+import Math.TEMP.HasEmpty
+import Math.TEMP.Collection
+import Math.Algebra.Monoid
 
 class Tree t where
   type Node t
@@ -53,7 +31,7 @@ class (Collection f, Tree(Element f), MultiplicativeMonoid f) => Forrest f where
   bplus :: Node (Element f) -> f -> Element f
   bminus :: Element f -> f
   mk_ :: Node(Element f) -> f -> f -> f
-  mk_ c x y = x ⋅ bplus c y
+  mk_ c x y = x * (singelton (bplus c y))
   mkSplit :: f -> (Node(Element f),f,f)
   mkSplit w = (mkRoot w, mkLeft w, mkRigth w)
   mkRoot :: f -> Node(Element f)
@@ -62,25 +40,16 @@ class (Collection f, Tree(Element f), MultiplicativeMonoid f) => Forrest f where
   mkLeft (mkSplit -> (_,l,_)) = l
   mkRigth :: f -> f
   mkRigth (mkSplit -> (_,_,r)) = r
-  mkT :: Node (Element f) -> FreeModule k f ⨂ FreeModule k f -> FreeModule k f
-  mkT c = linear (\(x,y) -> basis (mk_ c x y))
-  mk :: Node(Element f) -> FreeModule k f -> FreeModule k f -> FreeModule k f
-  mk c = bilinear mk_ c
   --unconcat :: f -> (Tree f, f)
 
+  -- mkT :: Node (Element f) -> FreeModule k f ⨂ FreeModule k f -> FreeModule k f
+  -- mkT c = linear (\(x,y) -> basis (mk_ c x y))
+
+
+-- mk :: (Forrest f) => Node(Element f) -> FreeModule k f -> FreeModule k f -> FreeModule k f
+-- mk c = bilinear (mk_ c)
 
 data MK c = I | MK {root:: c, left:: MK c, rigth :: MK c} deriving (Eq, Ord, Show)
-
-instance (Eq c) => HasEmpty (MK c) where
-  empty = I
-
--- instance (Eq c) => Collection (MK c) where
---   type Element (MK c) = MK c
-
-
--- instance (Forrest f) => Monoid (f c) where
---   mempty = empty
---   mappend = concat
 
 -- class GraftingProduct f where
 --   graft_, (⊵) :: f → f → FreeModule k f
