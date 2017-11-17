2a8ab3ab9383755aedf7e3f70db5a18b17485af2
Kristoffer Føllesdal <kfollesdal@gmail.com>
2017-11-17 13:36:26 +0100

    Some working on Lean.hs

--- src/Math/TEMP/Lean.hs
+++ src/Math/TEMP/Lean.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE
+  NoImplicitPrelude,
+  TypeApplications,
+  ScopedTypeVariables
+#-}
+
 import Test.LeanCheck
+import Test.LeanCheck.Function.ListsOfPairs
+import Math.Algebra.Properties.Elements
+import GHC.Base (Int (..), Bool (..), IO (..), Eq (..), ($))
+import Prelude (uncurry)
+import Control.Monad (mapM_)
+import GHC.Show (Show (..))
+import Math.Algebra.Monoid
+import Math.Algebra.Properties.Operations
 
 data Mk a = I | Mk a (Mk a) (Mk a) deriving (Show)
 
@@ -10,3 +24,13 @@ instance Listable A where
 instance Listable a => Listable (Mk a) where
   tiers = cons3 Mk
        \/ cons0 I
+
+data Type' a = Test'
+
+-- test :: forall a . (Listable a, AddidtativeMonoid a, Show a, Eq a) => Type' a -> IO()
+-- test _ = mapM_ check [unit @a (+) zero, assosiative @(a->a) (+)]
+
+testf :: forall a . (Listable a, AddidtativeMonoid a, Show a, Eq a) => Type' a -> IO()
+testf _ = check (assosiative @a (+))
+
+testInt = testf (Test' :: Type' Int)
