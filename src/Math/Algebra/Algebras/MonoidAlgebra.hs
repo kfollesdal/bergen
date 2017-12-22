958a8e64960610178b7aa35132fc828460766762
Kristoffer Føllesdal <kristoffer@Kristoffers-iMac.local>
2017-12-22 10:23:49 +0100

    Some Algebra testing

--- src/Math/Algebra/Algebras/MonoidAlgebra.hs
+++ src/Math/Algebra/Algebras/MonoidAlgebra.hs
@@ -4,23 +4,46 @@
   NoImplicitPrelude
 #-}
 
-module Math.Algebra.Algebra.MonoidAlgebra where
+module Math.Algebra.Algebras.MonoidAlgebra where
 
-import Math.Algebra
-import Math.Monoid
-import Math.Ring
-import Math.Module
-import Math.Module.FreeModule
-import GHC.Base (Eq (..), Ord (..))
+import Math.Algebra.Algebra
+import Math.Algebra.Monoid
+import Math.Algebra.Ring
+import Math.Algebra.Module
+import Math.Algebra.Module.FreeModule
+import GHC.Base (Eq (..), Ord (..), (++))
 
-monoidAlge :: (MultiplicativeMonoid b, CommutativeRing k, Eq k, Ord b) => AlgebraD (FreeModule k b)
-monoidAlgebra = AlgebraD {
-  unitD = \x -> x *> basis u,
-  multD = linear (\(x,y) -> basis (x*y))
+import Math.Algebra.Coalgebra
+import Math.Algebra.Bialgebra
+import Math.Algebra.HopfAlgebra
+
+monoidAlg :: (MultiplicativeMonoid b, CommutativeRing k, Eq k, Ord b) => AlgebraD (FModule k b)
+monoidAlg = AlgebraD {
+  unitD = \x -> x *> i u,
+  multD = linear (\(x,y) -> i (x*y))
 }
 
-graftAlg :: (CommutativeRing k) => AlgebraD (ForrestModule k b)
-gaftAlg = AlgebraD {
-  unitD = ,
-  multD = graft
+cotest :: (CommutativeRing k, Ord b, Eq k) => CoalgebraD (FModule k b)
+cotest = CoalgebraD {
+  counitD = \x -> u,
+  comultD = linear (\x -> te_ x x)
 }
+
+bitest :: (CommutativeRing k, MultiplicativeMonoid b, Ord b, Eq k) => BialgebraD (FModule k b)
+bitest = BialgebraD {
+  algebraD = monoidAlg,
+  coalgebraD = cotest
+}
+
+hopftest :: (CommutativeRing k, MultiplicativeMonoid b, Ord b, Eq k) => HopfAlgebraD (FModule k b)
+hopftest = HopfAlgebraD {
+  bialgebraD = bitest,
+  antipodeD = \x -> x
+}
+
+
+-- graftAlg :: (CommutativeRing k) => AlgebraD (ForrestModule k b)
+-- gaftAlg = AlgebraD {
+--   unitD = ,
+--   multD = graft
+-- }
