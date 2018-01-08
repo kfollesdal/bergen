2832f5b70b8d08b7001df0e58fa464509af087da
Kristoffer Føllesdal <kfollesdal@gmail.com>
2018-01-08 12:59:53 +0100

    Root-> R in treedoc.hs

--- src/Math/doc/treedoc.hs
+++ src/Math/doc/treedoc.hs
@@ -15,10 +15,12 @@ import GHC.Base (($), fmap)
 import Data.List (map)
 
 
-t1 = Root blue [Root black [Root black []], Root red [], Root green []]
-t2 = Root blue [Root red [], Root black [Root black []], Root green []]
-t3 = Root blue [Root red [], Root green [], Root black [Root black []]]
+t1 = R blue [R black [R black []], R red [], R green []]
+t2 = R blue [R red [], R black [R black []], R green []]
+t3 = R blue [R red [], R green [], R black [R black []]]
 
 out = draw (lcomb $ map (\(x,y) -> (NP x,y)) [(t1,1),(t2,6),(t3,1)]) "out.svg"
 
-out2 = draw (graft_ [NP $Root blue []] [NP $ Root black [Root black [], Root black []]]) "out.svg"
+out2 = draw (graft_ [NP $R blue []] [NP $ R black [R black [], R black []]]) "out.svg"
+
+out3 = draw (graft_ [R blue []] [R black [R black [], R black []]]) "out.svg"
